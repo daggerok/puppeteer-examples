89eefe84406eea0745aa94720efbb8e321c2eb69
Maksim Kostromin <daggerok@gmail.com>
2018-08-23 19:48:47 +0300

    init

---
+++ 01-getting-started/src/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+  <div className={'container'}>
+    <h1>Hello, puppeteer!</h1>
+  </div>,
+  document.querySelector('#app')
+);
---
+++ 01-getting-started/src/main.test.js
@@ -0,0 +1,35 @@
+const puppeteer = require('puppeteer');
+
+const debugIsEnabled = () => (
+  process.env.NODE_ENV !== 'debug' ? {} : {
+    headless: false,
+    devtools: true,
+    slowMo: 250,
+    // executablePath: string,  // path to chrome executable instead of using chromium
+    // timeout: number,         // (30 seconds by default), 0 to disable timeout
+    // ignoreHTTPSErrors: bool, // skip any https errors which may popup during tests
+  });
+
+describe('on page load', () => {
+  test('h1 loads correctly', async () => {
+    let browser = await puppeteer.launch(debugIsEnabled());
+    let page = await browser.newPage();
+
+    await page.emulate({
+      viewport: {
+        width: 500,
+        height: 2400,
+      },
+      userAgent: '',
+    });
+
+    await page.goto('http://127.0.0.1:1234');
+
+    const innerHTML = await page
+      .$eval('.container > h1', e => e.innerHTML);
+
+    expect(innerHTML).toBe('Hello, puppeteer!');
+
+    await browser.close();
+  }, 16000);
+});

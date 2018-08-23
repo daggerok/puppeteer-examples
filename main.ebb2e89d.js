bd8dc7dc6a6783112cdc5762e32804382f85df66
Maksim Kostromin <daggerok@gmail.com>
2018-08-23 20:24:07 +0300

    Github Pages https://daggerok.github.io/puppeteer-examples/ deployment 2018-08-23

--- main.ebb2e89d.js
+++ main.ebb2e89d.js
@@ -29,4 +29,4 @@ parcelRequire=function(e,r,n,t){var i="function"==typeof parcelRequire&&parcelRe
 },{"./cjs/react-dom.production.min.js":"i17t"}],"epB2":[function(require,module,exports) {
 "use strict";var e=require("react"),r=u(e),t=require("react-dom");function u(e){return e&&e.__esModule?e:{default:e}}(0,t.render)(r.default.createElement("div",{className:"container"},r.default.createElement("h1",null,"Hello, puppeteer!")),document.querySelector("#app"));
 },{"react":"1n8/","react-dom":"NKHc"}]},{},["epB2"], null)
-//# sourceMappingURL=/puppeteer-examples/main.b4cf888a.map
+//# sourceMappingURL=/puppeteer-examples/main.ebb2e89d.map
